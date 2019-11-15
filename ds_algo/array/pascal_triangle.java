c2868c48a231f93e46fa105956e8594ca28cf027
Shivam Tripathi <shivamtripathi0108@gmail.com>
2019-11-15 10:44:09 +0530

    feat(array): Generate pascals triangle

---
+++ ds_algo/array/pascal_triangle.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> solve(int A) {
+        ArrayList<ArrayList<Integer>> pascalsTriangle = new ArrayList<>();
+
+        while(pascalsTriangle.size() < A) {
+            ArrayList<Integer> rowN = new ArrayList<>();
+            rowN.add(1);
+
+            if (pascalsTriangle.size() > 0) {
+                ArrayList<Integer> prevRow = pascalsTriangle.get(pascalsTriangle.size() - 1);
+                int prev = prevRow.get(0);
+                for (int i = 1; i < prevRow.size(); i++) {
+                    int pres = prevRow.get(i);
+                    rowN.add(prev + pres);
+                    prev = pres;
+                }
+
+                rowN.add(1);
+            }
+
+            pascalsTriangle.add(rowN);
+        }
+
+        return pascalsTriangle;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ArrayList<ArrayList<Integer>> pascalsTriangle = solution.solve(10);
+
+        for (ArrayList<Integer> row: pascalsTriangle) {
+            for (Integer i: row) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
