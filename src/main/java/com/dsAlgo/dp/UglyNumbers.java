aed3dfd9b29b4071d0da4e29044d8042c68dd4c3
Shivam Tripathi <shivamtripathi@Shivams-MacBook-Pro.local>
2020-12-10 13:21:40 +0530

    feat(dsAlgo/dp/UglyNumbers): Ugly numbers

--- src/main/java/com/dsAlgo/dp/UglyNumbers.java
+++ src/main/java/com/dsAlgo/dp/UglyNumbers.java
@@ -1,48 +1,51 @@
 package com.dsAlgo.dp;
 
+/*
+Ugly numbers are numbers whose only prime factors are 2, 3 or 5.
+The sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15... shows the first 11 ugly numbers. By convention, 1 is included.
+Given a number n, the task is to find n’th Ugly number.
+ */
+
 import java.util.*;
-import java.util.stream.*;
-import java.io.*;
 
 public class UglyNumbers {
-	static List<Integer> list = new ArrayList<>() {{ add(1); }};
-	static int[] index = new int[] {1, 1, 1};
-	static Map<Integer, Integer> indexMap = new HashMap<>(Map.of(2, 1, 3, 1, 5, 1));
+	List<Integer> list = new ArrayList<>(Collections.singletonList(1));
+	Map<Integer, Integer> indexMap = new HashMap<>();
 
-	static int  getMultiple(int number) {
-		return number * indexMap.get(number);
-	}
-
-	static int getNumber(int n) {
+	int getNumber(int n) {
 		if (n < list.size()) {
 			return list.get(n - 1);
 		}
 
+
 		while (list.size() < n) {
-			int toAdd = getMultiple(2), threeMultiple = getMultiple(3), fiveMultiple = getMultiple(5);
-			int activeNumber = 2;
-			if (threeMultiple < toAdd && threeMultiple < fiveMultiple) {
-				toAdd = threeMultiple;
-				activeNumber = 3;
-			} else if (fiveMultiple < toAdd) {
-				toAdd = fiveMultiple;
-				activeNumber = 5;
+			List<Integer> multiples = Arrays.asList(
+							2 * list.get(indexMap.getOrDefault(2, 0)),
+							3 * list.get(indexMap.getOrDefault(3, 0)),
+							5 * list.get(indexMap.getOrDefault(5, 0))
+			);
+			Integer toAdd = Integer.MAX_VALUE;
+			int index = -1;
+			for (int i = 0; i < multiples.size(); i++) {
+				if (Integer.compare(multiples.get(i), toAdd) == -1) {
+					toAdd = multiples.get(i);
+					index = i == 0 ? 2 : (i == 1 ? 3 : 5);
+				}
 			}
 
+			indexMap.merge(index, 1, Integer::sum);
 			if (!list.get(list.size() - 1).equals(toAdd)) {
 				list.add(toAdd);
 			}
-			indexMap.put(activeNumber, indexMap.get(activeNumber) + 1);
 		}
 
 		System.out.println(list);
-
 		return list.get(n - 1);
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int limit = sc.nextInt();
-		System.out.println(UglyNumbers.getNumber(limit));
+		System.out.println(">> " + new UglyNumbers().getNumber(limit));
 	}
 }
