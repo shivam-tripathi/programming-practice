6b1b477038fe4117dabe980f70b0a69099be3cb4
Shivam Tripathi <shivamtripathi@Shivams-MacBook-Pro.local>
2020-12-16 11:09:36 +0530

    feat(leet/easy): https://leetcode.com/problems/squares-of-a-sorted-array/

---
+++ src/main/java/com/leet/easy/SquaresOfSortedArray.java
@@ -0,0 +1,30 @@
+package com.leet.easy;
+
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+  public static int[] sortedSquares(int[] nums) {
+    int[] squares = new int[nums.length];
+    int index = nums.length - 1;
+
+    int low = 0, high = nums.length - 1;
+
+    while (low <= high) {
+      int lowItemSquare = nums[low] * nums[low];
+      int highItemSquare = nums[high] * nums[high];
+      if (lowItemSquare > highItemSquare) {
+        squares[index--] = lowItemSquare;
+        low++;
+      } else {
+        squares[index--] = highItemSquare;
+        high--;
+      }
+    }
+    return squares;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {-4,-1,0,3,10};
+    System.out.println(Arrays.toString(sortedSquares(nums)));
+  }
+}
