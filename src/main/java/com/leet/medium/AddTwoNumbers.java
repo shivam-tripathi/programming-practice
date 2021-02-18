61ebbf84ed7152e34435813353c2ca7fb87579c6
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-02-18 16:20:14 +0530

    feat(leet/medium): https://leetcode.com/problems/add-two-numbers/

--- src/main/java/com/leet/medium/AddTwoNumbers.java
+++ src/main/java/com/leet/medium/AddTwoNumbers.java
@@ -1,18 +1,31 @@
 package com.leet.medium;
+/*
+2. Add Two Numbers
+https://leetcode.com/problems/add-two-numbers/
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4] // 2->4->3 and 5->6->4
+Output: [7,0,8] // 2+5, 4+6, 3+4+carry
+Explanation: 342 + 465 = 807.
+
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ */
 
 import com.leet.common.ListNode;
 
 public class AddTwoNumbers {
-  static class ReturnValue {
-    ListNode node;
-    int carry;
-
-    ReturnValue(ListNode node, int carry) {
-      this.node = node;
-      this.carry = carry;
-    }
-  }
-
   public ListNode addTwoNumberUtil(ListNode l1, ListNode l2, int prevCarry) {
     if (l1 == null && l2 == null) {
       return prevCarry != 0 ? new ListNode(prevCarry) : null;
