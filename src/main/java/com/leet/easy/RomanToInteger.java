ca460cf76c522df7f109267d83a317bb8cc1a0c2
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-02-21 10:21:56 +0530

    feat(leet/easy): https://leetcode.com/problems/roman-to-integer/

---
+++ src/main/java/com/leet/easy/RomanToInteger.java
@@ -0,0 +1,95 @@
+package com.leet.easy;
+
+/**
+ * 13. Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/
+ *
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply
+X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same
+principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9.
+    X can be placed before L (50) and C (100) to make 40 and 90.
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+
+Given a roman numeral, convert it to an integer.
+
+
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+
+Example 2:
+
+Input: s = "IV"
+Output: 4
+
+Example 3:
+
+Input: s = "IX"
+Output: 9
+
+Example 4:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 5:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+
+Constraints:
+
+    1 <= s.length <= 15
+    s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+    It is guaranteed that s is a valid roman numeral in the range [1, 3999]
+ */
+
+class RomanToTnteger {
+  public int charToValue(char c) {
+    switch(c) {
+      case 'I': return 1;
+      case 'V': return 5;
+      case 'X': return 10;
+      case 'L': return 50;
+      case 'C': return 100;
+      case 'D': return 500;
+      case 'M': return 1000;
+      default: return 0;
+    }
+  }
+  public int romanToInt(String s) {
+    int prev = 0, pres = 0, total = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      int value = charToValue(c);
+      total += value;
+      if (prev < value) {
+        total = total - 2 * prev;
+      }
+      prev = value;
+    }
+    return total;
+  }
+}
