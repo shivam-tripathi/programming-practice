d5887e0cc333316b9be4ed463acf24a5b7c4806a
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-05-14 13:42:55 +0530

    feat(practice/spoj): https://www.spoj.com/problems/RENT/

---
+++ src/main/java/com/practice/spoj/RENT.java
@@ -0,0 +1,122 @@
+package com.practice.spoj;
+
+/**
+ * RENT - Rent your airplane and make money
+ * https://www.spoj.com/problems/RENT/
+ * #sorting #dynamic-programming #binary-search
+ *
+ * "ABEAS Corp." is a very small company that owns a single airplane. The customers of ABEAS Corp are large airline
+ * companies which rent the airplane to accommodate occasional overcapacity.
+ *
+ * Customers send renting orders that consist of a time interval and a price that the customer is ready to pay for
+ * renting the airplane during the given time period. Orders of all the customers are known in advance. Of course, not
+ * all orders can be accommodated and some orders have to be declined. Eugene LAWLER, the Chief Scientific Officer of
+ * ABEAS Corp would like to maximize the profit of the company.
+ *
+ * You are requested to compute an optimal solution.
+ * Small Example
+ *
+ * Consider for instance the case where the company has 4 orders:
+ *
+ *     Order 1 (start time 0, duration 5, price 10)
+ *     Order 2 (start time 3, duration 7, price 8)
+ *     Order 3 (start time 5, duration 9, price 7)
+ *     Order 4 (start time 6, duration 9, price 8)
+ *
+ * The optimal solution consists in declining Order 2 and 3 and the gain is 10+8 = 18.
+ * Note that the solution made of Order 1 and 3 is feasible (the airplane is rented with no interruption from time 0 to
+ * time 14) but non-optimal.
+ * Input
+ *
+ * The first line of the input contains a number T ≤ 30 that indicates the number of test cases to follow. The first line
+ * of each test case contains the number of orders n (n ≤ 10000). In the following n lines the orders are given. Each
+ * order is described by 3 integer values: The start time of the order st (0 ≤ st < 1000000), the duration d of the
+ * order (0 < d < 1000000), and the price p (0 < p < 100000) the customer is ready to pay for this order.
+ * Output
+ *
+ * You are required to compute an optimal solution. For each test case your program has to write the total price paid by
+ * the airlines.
+ * Example
+ *
+ * Input:
+ * 1
+ * 4
+ * 0 5 10
+ * 3 7 14
+ * 5 9 7
+ * 6 9 8
+ *
+ * Output:
+ * 18
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class RENT {
+  static List<int[]> inputs;
+  static int[] dp;
+
+  // 456
+  static int greaterEqualTo(int val) {
+    int low = 0, high = inputs.size() - 1;
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      if (inputs.get(mid)[0] > val) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return low;
+  }
+
+  // 3 3 6 14
+  static int solve(int pos) {
+    if (pos >= inputs.size()) return 0;
+    if (dp[pos] != 0) return dp[pos];
+    var inp = inputs.get(pos);
+    int endTime = inp[0] + inp[1];
+    int nextPos = greaterEqualTo(endTime);
+    var nextInp = inputs.get(nextPos);
+    int with = inp[2] + (nextInp[0] > endTime ? solve(nextPos) : 0);
+    int without = solve(pos + 1);
+    dp[pos] = Math.max(with, without);
+    return dp[pos];
+  }
+
+  static int solve() {
+    dp = new int[inputs.size()];
+    return solve(0);
+  }
+
+  static void test() {
+    inputs = List.of(
+            new int[]{0, 5, 10},
+            new int[]{3, 7, 14},
+            new int[]{5, 9, 7},
+            new int[]{6, 9, 8}
+    );
+    System.out.println(solve());
+  }
+
+  public static void main(String[] args) throws IOException {
+//    test();
+    var obj = new BufferedReader(new InputStreamReader(System.in));
+    int tests = Integer.parseInt(obj.readLine());
+    for (int test = 0; test < tests; test++) {
+      int orders = Integer.parseInt(obj.readLine());
+      inputs = new ArrayList<>();
+      for (int order = 0; order < orders; order++) {
+        String[] inp = obj.readLine().split(" ");
+        inputs.add(new int[]{Integer.parseInt(inp[0]), Integer.parseInt(inp[1]), Integer.parseInt(inp[2])});
+      }
+      inputs.sort(Comparator.comparingInt(a -> a[0]));
+      System.out.println(solve());
+    }
+  }
+}
