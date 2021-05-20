af1d094c511eaf7962be67a73608eda80de1c8de
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-05-20 09:52:33 +0530

    feat(leet/medium): https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/

---
+++ src/main/java/com/leet/medium/MinimumMovesToEqualArrayElementsII.go
@@ -0,0 +1,19 @@
+package main
+
+func minMoves2(nums []int) int {
+  sort.Ints(nums)
+  median := nums[len(nums)/2]
+  if len(nums) % 2 != 1 {
+    median = (median + nums[len(nums)/2-1])/2
+  }
+  ans := 0
+  for _, item := range nums {
+    val := item - median
+    if val < 0 {
+      ans += -val
+    } else {
+      ans += val
+    }
+  }
+  return ans
+}
