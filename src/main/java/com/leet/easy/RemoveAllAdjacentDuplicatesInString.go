93159b26c346b8cf76fdfef1dc3d84c319f14392
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-11-10 19:32:13 +0530

    feat(leet/easy): https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/

--- src/main/java/com/leet/easy/RemoveAllAdjacentDuplicatesInString.go
+++ src/main/java/com/leet/easy/RemoveAllAdjacentDuplicatesInString.go
@@ -1,21 +1,23 @@
 package main
 
+import "strings"
+
 func removeDuplicates(s string) string {
-  stack := make([]rune, len(s))
-  size := 0
-  for _, char := range(s) {
-    if size == 0 || stack[size-1] != char {
-      stack[size] =  char
-      size++
-    } else {
-      for size > 0 && stack[size-1] == char {
-        size--
-      }
-    }
-  }
-  sb := strings.Builder{}
-  for i := 0; i < size; i++ {
-    sb.WriteRune(stack[i])
-  }
-  return sb.String()
+	stack := make([]rune, len(s))
+	size := 0
+	for _, char := range s {
+		if size == 0 || stack[size-1] != char {
+			stack[size] = char
+			size++
+		} else {
+			for size > 0 && stack[size-1] == char {
+				size--
+			}
+		}
+	}
+	sb := strings.Builder{}
+	for i := 0; i < size; i++ {
+		sb.WriteRune(stack[i])
+	}
+	return sb.String()
 }
