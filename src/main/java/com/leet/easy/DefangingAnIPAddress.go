9d3d60f9585a87d9659dfa0f5805c17c5614f3c9
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-11-10 19:09:00 +0530

    feat(leet/easy): https://leetcode.com/problems/defanging-an-ip-address/

--- src/main/java/com/leet/easy/DefangingAnIPAddress.go
+++ src/main/java/com/leet/easy/DefangingAnIPAddress.go
@@ -1,13 +1,7 @@
 package main
 
+import "strings"
+
 func defangIPaddr(address string) string {
-  builder := strings.Builder{}
-  for _, ch := range address {
-    if ch == '.' {
-      builder.WriteString("[.]")
-    } else {
-      builder.WriteRune(ch)
-    }
-  }
-  return builder.String()
+	strings.ReplaceAll(address, ".", "[.]")
 }
--- src/main/java/com/leet/easy/DefangingAnIPAddress.java
+++ src/main/java/com/leet/easy/DefangingAnIPAddress.java
@@ -3,43 +3,28 @@ package com.leet.easy;
 /**
  * 1108. Defanging an IP Address
  * https://leetcode.com/problems/defanging-an-ip-address/
+ *
  * Easy
  *
- * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+ * Given a valid (IPv4) IP address, return a defanged version of that IP
+ * address.
  *
  * A defanged IP address replaces every period "." with "[.]".
  *
- *
- *
  * Example 1:
  *
- * Input: address = "1.1.1.1"
- * Output: "1[.]1[.]1[.]1"
+ * Input: address = "1.1.1.1" Output: "1[.]1[.]1[.]1"
  *
  * Example 2:
  *
- * Input: address = "255.100.50.0"
- * Output: "255[.]100[.]50[.]0"
+ * Input: address = "255.100.50.0" Output: "255[.]100[.]50[.]0"
  *
+ * Constraints: The given address is a valid IPv4 address.
  *
- *
- * Constraints:
- *
- *     The given address is a valid IPv4 address.
- *
- * Accepted
- * 334,157
- * Submissions
- * 377,715
  */
 
 public class DefangingAnIPAddress {
   public String defangIPaddr(String address) {
-    var sb = new StringBuilder();
-    for (char ch : address.toCharArray()) {
-      if (ch == '.') sb.append("[.]");
-      else sb.append(ch);
-    }
-    return sb.toString();
+    return address.replaceAll("\\.", "[.]");
   }
 }
