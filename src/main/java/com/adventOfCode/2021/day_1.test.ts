4af5504413d33104dbc316d2e3f1df738be71df0
Shivam Tripathi <shivamtripathi0108@gmail.com>
2021-12-04 11:08:28 +0530

    feat(adventOfCode/2021): Day 1 Part 2

--- src/main/java/com/adventOfCode/2021/day_1.test.ts
+++ src/main/java/com/adventOfCode/2021/day_1.test.ts
@@ -1,35 +1,53 @@
-import { sonarSweep } from "./day_1.ts";
+import { sonarSweep, sonarSweepSlidingWindow } from "./day_1.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
+const SONAR_DEPTHS = [
+  199,
+  200,
+  208,
+  210,
+  200,
+  207,
+  240,
+  269,
+  260,
+  263,
+];
+
+const text = await Deno.readTextFile(
+  new URL("./input/sonar_sweep.txt", import.meta.url),
+);
+const depths = text.split("\n").map((s) => parseInt(s, 10));
+
 Deno.test({
-  name: "# Day 1: Sonar Sweep Test Input",
+  name: "# Day 1 Part 1: Sonar Sweep",
   fn: async () => {
-    const result = await sonarSweep([
-      199,
-      200,
-      208,
-      210,
-      200,
-      207,
-      240,
-      269,
-      260,
-      263,
-    ]);
+    const result = sonarSweep(SONAR_DEPTHS);
     assertEquals(result, 7);
   },
 });
 
 Deno.test({
-  name: "# Day 1: Sonar Sweep Original Input",
+  name: "# Day 1 Part 1: Sonar Sweep {{Original Input}}",
   fn: async () => {
-    const ans = await sonarSweep(
-      (await Deno.readTextFile(
-        new URL("./input/sonar_sweep.txt", import.meta.url),
-      ))
-        .split("\n")
-        .map((s) => parseInt(s, 10)),
-    );
+    const ans = sonarSweep(depths);
+    console.log({ ans });
+  },
+});
+
+Deno.test({
+  name: "# Day 1 Part 2: Sonar Sweep Sliding Window",
+  fn: () => {
+    const ans = sonarSweepSlidingWindow(SONAR_DEPTHS);
+    console.log(ans);
+    assertEquals(ans, 5);
+  },
+});
+
+Deno.test({
+  name: "# Day 1 Part 2: Sonar Sweep Sliding Window {{Original Input}}",
+  fn: async () => {
+    const ans = sonarSweepSlidingWindow(depths);
     console.log({ ans });
   },
 });
--- src/main/java/com/adventOfCode/2021/day_1.ts
+++ src/main/java/com/adventOfCode/2021/day_1.ts
@@ -1,7 +1,29 @@
-// deno run --allow-read=$(pwd) day_1_sonar_sweep.ts
+// deno run --allow-read=$(pwd) day_1.ts
 
-export async function sonarSweep(nums: number[]) {
-  return nums.reduce((acc, depth, index, depths) => {
+export function sonarSweep(depths: number[]) {
+  return depths.reduce((acc, depth, index, depths) => {
     return (index != 0 && depth > depths[index - 1]) ? acc + 1 : acc;
   }, 0);
+}
+
+export function sonarSweepSlidingWindow(
+  depths: number[],
+  windowSize = 3,
+): number {
+  const windows: Array<number> = [];
+
+  let running = 0;
+  if (windowSize <= depths.length) {
+    for (let i = 0; i < windowSize - 1; i++) {
+      running += depths[i];
+    }
+  }
+
+  for (let i = 2; i < depths.length; i++) {
+    running += depths[i];
+    windows.push(running);
+    running -= depths[i - 2];
+  }
+
+  return sonarSweep(windows);
 }
