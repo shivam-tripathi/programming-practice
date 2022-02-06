860e482b6cc3cd0998b1fb64291a2c6b15e1a0af
Shivam Tripathi <shivamtripathi0108@gmail.com>
2022-02-06 18:54:46 +0530

    feat(leet/hard): https://leetcode.com/problems/binary-tree-cameras/

---
+++ src/main/java/com/leet/hard/BinaryTreeCameras.go
@@ -0,0 +1,62 @@
+package main
+
+/*
+968. Binary Tree Cameras
+https://leetcode.com/problems/binary-tree-cameras/
+Hard
+
+You are given the root of a binary tree. We install cameras on the tree nodes where each camera at a
+node can monitor its parent, itself, and its immediate children.
+
+Return the minimum number of cameras needed to monitor all nodes of the tree.
+
+Example 1:
+Input: root = [0,0,null,0,0]
+Output: 1
+Explanation: One camera is enough to monitor all nodes if placed as shown.
+
+Example 2:
+Input: root = [0,0,null,0,null,0,null,null,0]
+Output: 2
+Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows
+one of the valid configurations of camera placement.
+
+Constraints:
+    The number of nodes in the tree is in the range [1, 1000].
+    Node.val == 0
+*/
+
+const (
+	COVERED = iota
+	REQUIRED
+	OWNS
+)
+
+func countCameras(node *TreeNode) (int, int) {
+	if node == nil {
+		return COVERED, 0
+	}
+
+	left, camerasOnLeft := countCameras(node.Left)
+	right, camerasOnRight := countCameras(node.Right)
+
+	if left == REQUIRED || right == REQUIRED {
+		return OWNS, camerasOnLeft + camerasOnRight + 1
+	}
+
+	if left == OWNS || right == OWNS {
+		return COVERED, camerasOnLeft + camerasOnRight
+	}
+
+	return REQUIRED, camerasOnLeft + camerasOnRight
+}
+
+func minCameraCover(root *TreeNode) int {
+	req, count := countCameras(root)
+
+	if req == REQUIRED {
+		return count + 1
+	}
+
+	return count
+}
