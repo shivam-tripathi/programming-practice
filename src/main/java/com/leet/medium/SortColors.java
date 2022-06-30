e315168af05be9e0ea6707709703fe3bc19c8839
Shivam Tripathi <shivamtripathi0108@gmail.com>
2022-06-30 16:41:58 +0530

    feat(leet/medium): https://leetcode.com/problems/sort-colors/

---
+++ src/main/java/com/leet/medium/SortColors.java
@@ -0,0 +1,20 @@
+package com.leet.medium;
+
+class Solution {
+  void swap(int[] nums, int a, int b) {
+    int tmp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = tmp;
+  }
+
+  public void sortColors(int[] nums) {
+    int zero = 0, two = nums.length-1, ptr = 0;
+    while (ptr <= two) {
+      switch (nums[ptr]) {
+        case 0: swap(nums, zero++, ptr++); break;
+        case 1: ptr++; break;
+        case 2: swap(nums, two--, ptr); break;
+      }
+    }
+  }
+}
