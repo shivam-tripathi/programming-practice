491370050a29242edf7ccc86aa048a98ae82772f
Shivam Tripathi <shivamtripathi0108@gmail.com>
2022-07-05 12:38:18 +0530

    feat(leet/medium): https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/

---
+++ src/main/java/com/leet/medium/MaximumBagsWithFullCapacityOfRocks.cpp
@@ -0,0 +1,69 @@
+#include <vector>
+using namespace std;
+
+/*
+2279. Maximum Bags With Full Capacity of Rocks
+https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/
+Medium
+
+You have n bags numbered from 0 to n - 1. You are given two 0-indexed integer arrays
+capacity and rocks. The ith bag can hold a maximum of capacity[i] rocks and currently
+contains rocks[i] rocks. You are also given an integer additionalRocks, the number of
+additional rocks you can place in any of the bags.
+
+Return the maximum number of bags that could have full capacity after placing the
+additional rocks in some bags.
+
+Example 1:
+Input: capacity = [2,3,4,5], rocks = [1,2,4,4], additionalRocks = 2
+Output: 3
+Explanation:
+Place 1 rock in bag 0 and 1 rock in bag 1.
+The number of rocks in each bag are now [2,3,4,4].
+Bags 0, 1, and 2 have full capacity.
+There are 3 bags at full capacity, so we return 3.
+It can be shown that it is not possible to have more than 3 bags at full capacity.
+Note that there may be other ways of placing the rocks that result in an answer of 3.
+
+Example 2:
+Input: capacity = [10,2,2], rocks = [2,2,0], additionalRocks = 100
+Output: 3
+Explanation:
+Place 8 rocks in bag 0 and 2 rocks in bag 2.
+The number of rocks in each bag are now [10,2,2].
+Bags 0, 1, and 2 have full capacity.
+There are 3 bags at full capacity, so we return 3.
+It can be shown that it is not possible to have more than 3 bags at full capacity.
+Note that we did not use all of the additional rocks.
+
+Constraints:
+		n == capacity.length == rocks.length
+		1 <= n <= 5 * 104
+		1 <= capacity[i] <= 109
+		0 <= rocks[i] <= capacity[i]
+		1 <= additionalRocks <= 109
+*/
+
+class Solution
+{
+public:
+	int maximumBags(vector<int> &capacity, vector<int> &rocks, int additionalRocks)
+	{
+		vector<int> diffs(rocks.size());
+		for (int i = 0; i < rocks.size(); i++)
+		{
+			diffs[i] = capacity[i] - rocks[i];
+		}
+
+		sort(diffs.begin(), diffs.end());
+
+		int ptr = 0, left = additionalRocks;
+
+		while (ptr < rocks.size() && left >= 0)
+		{
+			left -= diffs[ptr++];
+		}
+
+		return left < 0 ? ptr - 1 : ptr;
+	}
+};
